335504c3f9ff573955d6c926550c58b215707e80
Mike Belin <codxxx@gmail.com>
2020-03-05 00:08:13 +0300

    Практика №3 - Коллекции

---
+++ src/main/java/ru/mbelin/hw4/TestChat.java
@@ -0,0 +1,31 @@
+package ru.mbelin.hw4;
+
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class TestChat {
+
+    public static void main(String[] args) {
+
+        SwingUtilities.invokeLater(()->{
+            NetChatForm netChatForm = new NetChatForm();
+            List<String> list = new ArrayList<>();
+            list.add("Mike"); list.add("Ivan"); list.add("Anna");
+            netChatForm.loadListContacts(list);
+        });
+
+       /*
+        //https://javarush.ru/groups/posts/regulyarnye-vyrazheniya-v-java
+        String s = "@Mike: Привет";
+        Pattern pattern = Pattern.compile("@\\w+?:");
+        Matcher matcher = pattern.matcher(s);
+        while (matcher.find()) {
+            System.out.println(s.substring(matcher.start(), matcher.end()));
+            System.out.println(s.substring(matcher.end(), s.length()));
+        }
+        */
+    }
+}
